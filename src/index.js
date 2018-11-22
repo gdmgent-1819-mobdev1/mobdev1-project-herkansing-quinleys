c6c8e5664dcabe371560910da8bdc240ab90753a
Lester Lievens <lester.lievens@arteveldehs.be>
2018-11-22 14:29:43 +0100

    Fixed wrong routing bug and removed document.click

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Navigo from 'navigo';
 import handlebars, { compile } from 'handlebars';
 import './styles/main.sass';
+
 import routes from './routes';
 
 // Partials
@@ -17,6 +18,7 @@ const router = new Navigo(window.location.origin, true);
 routes.forEach((route) => {
   router.on(route.path, () => {
     route.view();
+    router.updatePageLinks();
   });
 });
 
@@ -26,11 +28,5 @@ router.notFound(() => {
 });
 router.resolve();
 window.onload = () => {
-  document.onclick = (e) => {
-    e.preventDefault();
-    const target = e.target.getAttribute('href');
-    if (target != null) {
-      router.navigate(target);
-    }
-  };
+  router.navigate(window.location.hash.split('/')[1]);
 };
